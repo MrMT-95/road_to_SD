2e9c8751181f7888c7b89c19fb86ad24e25b68fb
MrMT-95 <qanteee@gmail.com>
2021-04-28 23:00:34 +0200

    comments

--- src/com/company/BucketSort.java
+++ src/com/company/BucketSort.java
@@ -7,29 +7,35 @@ public class BucketSort {
 
     static void bucketSort(double[] table, int size) {
 
+        //if worth check
         if (size <= 1) {
             return;
         }
 
         int i;
 
+        //def bucket
         ArrayList<Double>[] bucket = new ArrayList[size];
 
+        //create buckets
         for (i = 0; i < size; i++) {
             bucket[i] = new ArrayList<>();
         }
 
+        //pop elements to buckets matching the range
         for (i = 0; i < size; i++) {
             int bucketIndex = (int) (table[i] * size);
             bucket[bucketIndex].add(table[i]);
         }
 
+        //insertion sorting
         for (i = 0; i < size; i++) {
             bucket[i].sort(Comparator.naturalOrder());
         }
 
         int tableIndex=0;
 
+        //transfer sorted elements from buckets to table
         for (i = 0; i < size; i++) {
             if (!bucket[i].isEmpty()) {
                 for (int j = 0; j < bucket[i].size(); j++) {
--- src/com/company/QuickSort.java
+++ src/com/company/QuickSort.java
@@ -4,20 +4,26 @@ public class QuickSort {
 
 
     static void quickSort(int[] table, int left, int right) {
+        //set left and right boundary
         int i = left;
         int j = right;
 
+        //create middle point
         int pivot = table[(right + left) / 2];
 
         do {
+            //looking for greater element than pivot
             while (table[i] < pivot) {
                 i++;
             }
-
+            //looking for smaller element than pivot
             while (table[j] > pivot) {
                 j--;
             }
 
+            //swapping greater and smaller elements
+            // to have smaller elements on left side of pivot
+            // and greater on right side of pivot
             if (i <= j) {
                 int swap = table[i];
                 table[i] = table[j];
@@ -27,13 +33,14 @@ public class QuickSort {
                 j--;
             }
 
-
         } while (i <= j);
 
+        //sorting left table side
         if (left<j){
             quickSort(table,left,j);
         }
 
+        //sorting right table side
         if (right>i){
             quickSort(table,i,right);
         }
