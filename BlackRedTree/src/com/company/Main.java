7bbe7a86aeefc9b55cd35964c4a29c7455d038e5
Mateusz Tarczyński <58234909+MrMT-95@users.noreply.github.com>
2021-06-01 10:12:26 +0200

    Add files via upload

---
+++ BlackRedTree/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Tree redBlackTree = new Tree();
+        redBlackTree.add(2);
+        redBlackTree.add(5);
+        redBlackTree.add(1);
+        redBlackTree.add(8);
+        System.out.println(redBlackTree.toString());
+
+    }
+}
